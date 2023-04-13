6746c4661864c5c8399711440d39f74c40c71c1b
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-13 10:40:31 +0300

    Write all RSpec tests for the application

--- spec/routing/generators_routing_spec.rb
+++ spec/routing/generators_routing_spec.rb
@@ -6,33 +6,8 @@ RSpec.describe GeneratorsController, type: :routing do
       expect(get: "/generators").to route_to("generators#index")
     end
 
-    it "routes to #new" do
-      expect(get: "/generators/new").to route_to("generators#new")
-    end
-
-    it "routes to #show" do
-      expect(get: "/generators/1").to route_to("generators#show", id: "1")
-    end
-
-    it "routes to #edit" do
-      expect(get: "/generators/1/edit").to route_to("generators#edit", id: "1")
-    end
-
-
-    it "routes to #create" do
-      expect(post: "/generators").to route_to("generators#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/generators/1").to route_to("generators#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/generators/1").to route_to("generators#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/generators/1").to route_to("generators#destroy", id: "1")
+     it "root route to generators#index" do
+      expect(get: "/").to route_to("generators#index")
     end
   end
 end
