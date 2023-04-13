5b7a18f99df6c96e22fdcaf24230d110d588756f
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-13 11:16:00 +0300

    Fix linter errors

--- spec/routing/generators_routing_spec.rb
+++ spec/routing/generators_routing_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe GeneratorsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/generators").to route_to("generators#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/generators').to route_to('generators#index')
     end
 
-     it "root route to generators#index" do
-      expect(get: "/").to route_to("generators#index")
+    it 'root route to generators#index' do
+      expect(get: '/').to route_to('generators#index')
     end
   end
 end
--- spec/views/generators/index.html.erb_spec.rb
+++ spec/views/generators/index.html.erb_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe 'generators/index', type: :view do
   it 'displays the front-end developer test project page' do
     render
-    
+
     expect(rendered).to have_selector('section.d-flex.flex-column.align-items-center.justify-content-center.mt-5')
-    expect(rendered).to have_selector('h1.border-bottom.border-2.fs-1.fw-bold.pb-3', text: 'Front-end developer test project')
-    expect(rendered).to have_selector('p.main-txt', text: 'Your goal is to make a page that looks exactly this one, and has the ability to create H1 text simply by typing / then 1, then typing text, and hitting enter.')
+    expect(rendered).to have_selector('h1.border-bottom.border-2.fs-1.fw-bold.pb-3',
+                                      text: 'Front-end developer test project')
+    expect(rendered).to have_selector('p.main-txt',
+                                      text: 'Your goal is to make a page that looks exactly this one, and has the ability to create H1 text simply by typing / then 1, then typing text, and hitting enter.')
     expect(rendered).to have_selector('section#container-wrapper.d-flex.flex-column.align-items-center.justify-content-center.mt-5')
     expect(rendered).to have_selector('div#container.input-container.w-100')
     expect(rendered).to have_selector('input#text-input.w-100.mt-5.p-0.opacity-40.fs-5[type="text"][placeholder="Type /1 followed by text..."][autofocus]')
